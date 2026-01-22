990ccab7bd73fb32eb269e28999005be304bf6cf
wtRinaldi <williamtrinaldi@gmail.com>
2026-01-21 21:06:44 -0800

    fix pool

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.use(express.json());
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
 
 // GET all messages
