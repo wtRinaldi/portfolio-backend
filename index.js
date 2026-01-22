23612743e8a95f4fb021e856675af513d19a2e6f
wtRinaldi <williamtrinaldi@gmail.com>
2026-01-21 22:34:25 -0800

    convert to db url

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ app.use(express.json());
 // PostgreSQL connection
 // ---------------------
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false } // Required for Railway
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // required for Railway
 });
 
 // Debug: confirm env vars
